422174c0b10a5fe4e34570fcd27b3d0173c092e1
Judd Rollins <hjrolli2@ncsu.edu>
2020-07-06 22:29:36 -0400

    Added database functionality to server side

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 const app = express();
 
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
-database = []
+database = new Datastore('database.db')
+database.loadDatabase();
 
 app.post("/api", (request, response) => {
-  // console.log(request.body);
+  console.log("I got a request");
+  
   const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
 
-  database.push(data);
-  console.log(database)
+  database.insert(data);
 
   response.json({
     status:'success',
+    timestamp: data.timestamp,
     latitude: data.latitude,
     longitude: data.longitude
   });
