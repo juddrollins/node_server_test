c08b942cd073563ac72467fd4da3e833ac6d9b29
Judd Rollins <hjrolli2@ncsu.edu>
2020-07-06 22:14:41 -0400

    Added button to sent lat and long to server when pressed

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
+database = []
+
 app.post("/api", (request, response) => {
-  console.log(request.body);
+  // console.log(request.body);
   const data = request.body;
+
+  database.push(data);
+  console.log(database)
+
   response.json({
     status:'success',
     latitude: data.latitude,
